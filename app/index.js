134d0ce4e73300ed4c176b328d36a5bb5576f05f
Thiago <teyextreme@outlook.com>
2023-09-12 08:52:06 -0300

    SetPut

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return(
+        <View>
+            <Text>Callback</Text>
+            <Text>Uau</Text>
+        </View>
+    )
+}
+
+export default Home
