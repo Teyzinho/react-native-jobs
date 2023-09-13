ee5844d5554f23bf9d49c587c5d996e9f087c0eb
Thiago <teyextreme@outlook.com>
2023-09-13 10:12:27 -0300

    jobs Details

--- app/job-details/[id].jsx
+++ app/job-details/[id].jsx
@@ -21,14 +21,14 @@ import {
 } from "../../components";
 import { ScrollView } from "react-native-gesture-handler";
 
-const tabs = ['Sobre', 'Qualificações', 'Responsabilidades'];
+const tabs = ["Sobre", "Qualificações", "Responsabilidades"];
 
 const JobDetails = () => {
   const params = useGlobalSearchParams();
   const router = useRouter();
 
-  const [refreshing, setRefreshing] = useState(false)
-  const [activeTab, setActiveTab] = useState(tabs[0])
+  const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
   params.id;
 
@@ -36,9 +36,32 @@ const JobDetails = () => {
     job_id: params.id,
   });
 
-  const onRefresh = () => {
+  const onRefresh = () => {};
 
-  }
+  const displayTabContext = () => {
+    switch (activeTab) {
+      case tabs[0]:
+        return (
+          <JobAbout info={data[0].job_description ?? ["Sem informações"]} />
+        );
+      case tabs[1]:
+        return (
+          <Specifics
+            title="Qualificações"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+      case tabs[2]:
+        return (
+          <Specifics
+            title="Responsabilidades"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
+      default:
+        break;
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -63,19 +86,19 @@ const JobDetails = () => {
       <>
         <ScrollView
           showsVerticalScrollIndicator={false}
-          refreshControl={<RefreshControl refreshing={refreshing} 
-          onRefresh={onRefresh}
-          />}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
         >
           {isLoading ? (
-            <ActivityIndicator size="large" color={COLORS.primary}/>
+            <ActivityIndicator size="large" color={COLORS.primary} />
           ) : error ? (
             <Text>Algo deu Errado</Text>
-          ): data.length === 0 ? (
+          ) : data.length === 0 ? (
             <Text>Sem informações</Text>
           ) : (
-            <View style={{padding: SIZES.medium, paddingBottom: 100}}>
-              <Company 
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
                 companyLogo={data[0].employer_logo}
                 jobTitle={data[0].job_title}
                 companyName={data[0].employer_name}
@@ -86,9 +109,9 @@ const JobDetails = () => {
                 tabs={tabs}
                 activeTab={activeTab}
                 setActiveTab={setActiveTab}
-              />  
+              />
 
-
+              {displayTabContext()}
             </View>
           )}
         </ScrollView>
