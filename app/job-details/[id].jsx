7ab4432316eaa574785bae5214a340e86530b61e
Thiago <teyextreme@outlook.com>
2023-09-13 10:42:46 -0300

    update home

--- app/job-details/[id].jsx
+++ app/job-details/[id].jsx
@@ -36,7 +36,11 @@ const JobDetails = () => {
     job_id: params.id,
   });
 
-  const onRefresh = () => {};
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    refetch();
+    setRefreshing(false)
+  },[]);
 
   const displayTabContext = () => {
     switch (activeTab) {
